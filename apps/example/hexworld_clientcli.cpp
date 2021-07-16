c8fe22804ddc54d7888aba6cd0cb6363f057007f
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2021-07-16 10:59:44 +0100

    Changed the encrypted connection option into a flag (--nossl instead of --ssl false)

--- apps/example/hexworld_clientcli.cpp
+++ apps/example/hexworld_clientcli.cpp
@@ -6,7 +6,7 @@ namespace po = boost::program_options;
 
 int main(int ac, char** av) {
     std::string ServerAddress;
-    bool EncryptedConnection;
+    bool EncryptedConnection = true;
 
     try {
         po::options_description desc("Hexagon client options");
@@ -14,7 +14,7 @@ int main(int ac, char** av) {
                 ("help", "help message")
                 ("address", po::value<std::string>(&ServerAddress)->default_value("127.0.0.1:8080"),
                  "address to connect to [ip:port]")
-                ("ssl", po::value<bool>(&EncryptedConnection)->default_value(true), "Connect encrypted");
+                ("nossl","connect unencrypted");
 
         po::variables_map vm;
         po::store(po::parse_command_line(ac, av, desc), vm);
@@ -28,6 +28,11 @@ int main(int ac, char** av) {
         if(vm.count("address")) {
             ServerAddress = vm["address"].as<std::string>();
         }
+
+        if(vm.count("nossl")) {
+            EncryptedConnection = false;
+        }
+
     } catch(po::error& error) {
         std::cout << error.what() << std::endl;
         return 1;
